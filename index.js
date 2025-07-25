d1bbb855022ecbeaf4523fefd109541534f1cfde
knyazkin maxim <knyazkin.maxim.626@gmail.com>
2025-07-25 18:25:12 +0300

    Third commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-class Character {
-  constructor(name, type, health, level, attack, defence) {
+export class Character {
+  constructor(name, type) {
     if (typeof name !== "string" || name.length < 2 || name.length > 10) {
       throw new Error(
         "Имя должно быть строкой не менее 2-х символов и не более 10 символов"
@@ -22,53 +22,72 @@ class Character {
 
     this.health = 100;
     this.level = 1;
-
-    if (type === "Bowman") {
-      this.attack = 25;
-      this.defence = 25;
-    }
-    if (type === "Swordsman") {
-      this.attack = 40;
-      this.defence = 10;
-    }
-    if (type === "Magician") {
-      this.attack = 10;
-      this.defence = 40;
-    }
-    if (type === "Undead") {
-      this.attack = 25;
-      this.defence = 25;
-    }
-    if (type === "Zombie") {
-      this.attack = 40;
-      this.defence = 10;
-    }
-    if (type === "Daemon") {
-      this.attack = 10;
-      this.defence = 40;
-    }
   }
 
   levelUp() {
-    if (this.health !== 0) {
-      console.log("Уровень повышен");
+    if (this.health == 0) {
+      throw new Error("Нельзя повысить уровень умершего!");
+    }
+    console.log("Уровень повышен");
       this.level = this.level + 1;
       this.attack = this.attack * 1.2;
       this.defence = this.defence * 1.2;
       this.health = 100;
-    } else {
-      throw new Error("Нельзя повысить уровень умершего!");
-    }
   }
 
   damage(points) {
     if (this.health >= 0) {
       this.health = this.health - points * (1 - this.defence / 100);
-    } else {
-      throw new Error("Персонаж уже умер");
     }
+    throw new Error("Персонаж уже умер");
   }
 }
 
-export let Player1 = new Character("Bloody", "Undead");
-console.log(Player1);
+export class Bowman extends Character{
+  constructor(name) {
+    super(name, 'Bowman');
+    this.attack = 25;
+    this.defence = 25;
+  }
+}
+
+export class Swordsman extends Character{
+  constructor(name) {
+    super(name, 'Swordsman');
+    this.attack = 40;
+    this.defence = 10;
+  }
+}
+
+export class Magician extends Character{
+  constructor(name) {
+    super(name, 'Magician');
+    this.attack = 10;
+    this.defence = 40;
+  }
+}
+
+export class Undead extends Character{
+  constructor(name) {
+    super(name, 'Undead');
+    this.attack = 25;
+    this.defence = 25;
+  }
+}
+
+export class Zombie extends Character{
+  constructor(name) {
+    super(name, 'Zombie');
+    this.attack = 40;
+    this.defence = 10;
+  }
+}
+
+export class Daemon extends Character{
+  constructor(name) {
+    super(name, 'Daemon');
+    this.attack = 10;
+    this.defence = 40;
+  }
+}
+
--- test.js
+++ test.js
@@ -1,11 +1,59 @@
-import { Player1 } from "../index.js";
+import { Character, Bowman} from "./index.js";
 
-test("Player1 Health Check", () => {
-  let result = Player1.health;
+// Для примера экспортировал основной класс Character и дочерний Bowman.
+// Остальные дочерние не стал экспортировать
+
+let Player = new Character('Rikky', 'Daemon');
+
+test("Player Health Check = 100", () => {
+  let result = Player.health;
   expect(result).toBe(100);
 });
 
-test("Player1 Level Check", () => {
-  let result = Player1.level;
+test("Player Level Check = 1", () => {
+  let result = Player.level;
   expect(result).toBe(1);
 });
+
+test("Player Name Check = Rikky", () => {
+  let result = Player.name;
+  expect(result).toBe('Rikky');
+});
+
+test("Player type Check = Daemon", () => {
+  let result = Player.type;
+  expect(result).toBe('Daemon');
+});
+
+let bowman1 = new Bowman('Vasilisk');
+
+test("bowman1 Health Check = 100", () => {
+  let result = bowman1.health;
+  expect(result).toBe(100);
+});
+
+test("bowman1 Level Check = 1", () => {
+  let result = bowman1.level;
+  expect(result).toBe(1);
+});
+
+test("bowman1 Name Check = Vasilisk", () => {
+  let result = bowman1.name;
+  expect(result).toBe('Vasilisk');
+});
+
+test("bowman1 type Check = Bowman", () => {
+  let result = bowman1.type;
+  expect(result).toBe('Bowman');
+});
+
+test("bowman1 attack Check = 25", () => {
+  let result = bowman1.attack;
+  expect(result).toBe(25);
+});
+
+test("bowman1 defence Check = 25", () => {
+  let result = bowman1.defence;
+  expect(result).toBe(25);
+});
+
